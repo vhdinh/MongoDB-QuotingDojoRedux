d99e96a9afeaa2ad7863e160169464f250e5d7e9
Vu Dinh <isthis.vu@gmail.com>
2016-04-11 08:19:30 -0700

    added mongodb and test first db submit

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 // Require Express Module
 var express = require("express");
-
+var path = require("path");
 var app = express();
+var mongoose = require('mongoose');
 
-// require body-parser(to receive post data from clients)
-var bodyParser = require("body-parser")
+// connect mongodb database wusing mongoose - basic_mongoose is name of our db in mongodb - this name should match name of db we are using for project
+mongoose.connect('mongodb://localhost/quote_dojo');
 
+var UserSchema = new mongoose.Schema({
+	name: String,
+	quote: String,
+	date: Date
+})
+
+mongoose.model('Quote', UserSchema); //setting this Schema in our Models as "User"
+var Quote = mongoose.model('Quote') // retrieve this Schema from our Models named "User"
+
+
+// Require body-parser (to receive post data from clients)
+var bodyParser = require("body-parser");
+// Integrate body-parser with our App
 app.use(bodyParser.urlencoded());
 
-// require path
-var path = require("path");
 
 // Setting up static folder directory
-app.use(express.static(__dirname + "./static"));
+app.use(express.static(path.join(__dirname, "./static")));
 
 // Setting up views folder directory
 app.set("views", path.join(__dirname, "./views"));
@@ -26,6 +38,20 @@ app.get("/", function(req, res) {
 	res.render("index");
 })
 
+app.post('/quotes', function(req, res) {
+	console.log("POST DATA", req.body)
+	var quote = new Quote({name: req.body.name, quote: req.body.quote, date: Date()})
+	// console.log("QUOTEEE ", quote)
+	quote.save(function(err) {
+		if(err){
+			console.log("Something went wrong!!!")
+		}
+		else{
+			console.log("Successfully added a quote");
+			res.redirect("/")
+		}
+	})
+})
 
 
 // Setting our server to listen to port 8000
